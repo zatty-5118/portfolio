0018fc930fa3a5bfd119d7f044682a5b99dd1636
zatty-5118 <ryosuke.29.m@outlook.jp>
2024-10-04 13:35:14 +0900

    fixed theme-color

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
     <html lang="ja">
       <head>
         <link rel="icon" href="favicon.ico" />
-        <meta name="theme-color" content="#5780b3" />
+        <meta name="theme-color" content="#6699cc" />
         <meta property="og:image" content="https://raw.githubusercontent.com/zatty-5118/portfolio/main/public/images/ogImage.jpg" />
       </head>
       <body className={NotoSansJp.className}>
